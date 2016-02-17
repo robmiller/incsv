2fd080ae8d88f4e67e4b40fc59170da889005da8
Rob Miller <rob@bigfish.co.uk>
2016-02-17 22:45:15 +0000

    Column detection
    Given sample values, detects the type of data they probably represent.
    
    Currently handles ISO 8601 dates and USD/GBP currency; everything else
    is treated as a string.

---
+++ lib/incsv/column.rb
@@ -0,0 +1,79 @@
+require "bigdecimal"
+
+module InCSV
+  class Column
+    class ColumnType
+      def self.type
+        self.to_s.sub(/.*::/, "").downcase.to_sym
+      end
+
+      def self.db_type
+        self.to_s.sub(/.*::/, "").downcase.to_sym
+      end
+
+      def initialize(value)
+        @value = value
+      end
+
+      def match?
+        false
+      end
+
+      def clean_value
+        @value
+      end
+
+      private
+      attr_reader :value
+    end
+
+    module Types
+      class Date < ColumnType
+        def match?
+          value.strip.match(/\A[0-9]{4}-[0-9]{2}-[0-9]{2}\z/)
+        end
+      end
+
+      class Currency < ColumnType
+        MATCH_EXPRESSION = /\A(\$|£)([0-9,\.]+)\z/
+
+        def self.db_type
+          "DECIMAL(10,2)"
+        end
+
+        def match?
+          value.strip.match(MATCH_EXPRESSION)
+        end
+
+        def clean_value
+          value.strip.match(MATCH_EXPRESSION) do |match|
+            BigDecimal(match[2].delete(","))
+          end
+        end
+      end
+
+      class String < ColumnType
+        def match?
+          true
+        end
+      end
+    end
+
+    def initialize(values)
+      @values = values
+    end
+
+    def type
+      Types.constants.select do |column_type|
+        column_type = Types.const_get(column_type)
+        if values.any? { |value| column_type.new(value).match? }
+          return column_type.type
+        end
+      end
+    end
+
+    private
+
+    attr_accessor :values
+  end
+end
---
+++ spec/lib/incsv/column_spec.rb
@@ -0,0 +1,67 @@
+require_relative "../../spec_helper"
+
+module InCSV
+  describe Column do
+    describe Column::Types do
+      describe Column::Types::Date do
+        it "matches dates" do
+          expect(Column::Types::Date.new("2015-01-01").match?).to be_truthy
+        end
+      end
+
+      describe Column::Types::Currency do
+        it "matches dollars" do
+          expect(Column::Types::Currency.new("$4.90").match?).to be_truthy
+          expect(Column::Types::Currency.new("$4").match?).to be_truthy
+          expect(Column::Types::Currency.new("$4,806").match?).to be_truthy
+        end
+
+        it "matches pounds" do
+          expect(Column::Types::Currency.new("£4.90").match?).to be_truthy
+          expect(Column::Types::Currency.new("£4").match?).to be_truthy
+          expect(Column::Types::Currency.new("£4,806").match?).to be_truthy
+        end
+
+        it "treats currency as a fixed precision number in the database" do
+          expect(Column::Types::Currency.db_type).to eq("DECIMAL(10,2)")
+        end
+
+        it "strips currencies and just stores the number" do
+          expect(Column::Types::Currency.new("$4.90").clean_value).to eq(BigDecimal("4.9"))
+          expect(Column::Types::Currency.new("$4,690,300.90").clean_value).to eq(BigDecimal("4690300.9"))
+        end
+      end
+
+      describe Column::Types::String do
+        it "matches anything" do
+          expect(Column::Types::String.new("foo").match?).to be_truthy
+          expect(Column::Types::String.new(nil).match?).to be_truthy
+        end
+      end
+    end
+
+    describe "initialize" do
+      it "accepts a list of values" do
+        column = Column.new(["foo", "bar"])
+        expect(column).to be_a(Column)
+      end
+    end
+
+    describe "type" do
+      it "interprets ISO-format dates as dates" do
+        column = Column.new(["2015-01-01", "1994-04-01"])
+        expect(column.type).to eq(:date)
+      end
+
+      it "interprets dollar and pound values as currency" do
+        column = Column.new(["£3.90", "$4,909.20"])
+        expect(column.type).to eq(:currency)
+      end
+
+      it "interprets unknown formats as strings" do
+        column = Column.new(["foo", "bar"])
+        expect(column.type).to eq(:string)
+      end
+    end
+  end
+end
