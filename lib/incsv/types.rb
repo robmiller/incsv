16c000c87f8d04ce707fdc88db7710756b6459a2
Rob Miller <rob@bigfish.co.uk>
2016-03-23 22:39:43 +0000

    Add a note on load order and column type guessing

--- lib/incsv/types.rb
+++ lib/incsv/types.rb
@@ -1,5 +1,8 @@
 require "incsv/column_type"
 
+# The order that these files are required defines the priority order of
+# column guessing; they should therefore be required in order from most-
+# to least-specific (with string always last).
 require "incsv/types/date"
 require "incsv/types/currency"
 require "incsv/types/integer"
